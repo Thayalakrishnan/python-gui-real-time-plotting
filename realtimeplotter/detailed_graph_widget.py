f2c1ffa914a668cd47fe52cb0af5ef2ef5787b0d
Lawen <lawen.thaya94@gmail.com>
2022-08-09 00:41:54 +1000

    lazy commit

--- realtimeplotter/detailed_graph_widget.py
+++ realtimeplotter/detailed_graph_widget.py
@@ -60,45 +60,7 @@ class DetailedGraphWidget(QtWidgets.QWidget):
         self.run_scan.setFixedSize(120,50)
         self.stop_scan = QtWidgets.QPushButton(text="Stop Scan")
         self.stop_scan.setFixedSize(120,50)
-        
-        
-        #''' Accelerometer '''
-        #self.lcd_acc = QtWidgets.QLCDNumber()
-        #self.lcd_acc.setFrameShape(QtWidgets.QFrame.NoFrame)
-        #self.lcd_acc.setSmallDecimalPoint(False)
-        #self.lcd_acc.setObjectName("lcd_acc")
-        #self.lcd_acc.setFixedSize(120,50)
-        #''' Gyroscope '''
-        #self.lcd_gyr = QtWidgets.QLCDNumber()
-        #self.lcd_gyr.setFrameShape(QtWidgets.QFrame.NoFrame)
-        #self.lcd_gyr.setSmallDecimalPoint(False)
-        #self.lcd_gyr.setObjectName("lcd_gyr")
-        #self.lcd_gyr.setFixedSize(120,50)
-        #''' Magnetometer '''
-        #self.lcd_mag = QtWidgets.QLCDNumber()
-        #self.lcd_mag.setFrameShape(QtWidgets.QFrame.NoFrame)
-        #self.lcd_mag.setSmallDecimalPoint(False)
-        #self.lcd_mag.setObjectName("lcd_mag")
-        #self.lcd_mag.setFixedSize(120,50)
-        #''' Azimuth '''
-        #self.lcd_azi = QtWidgets.QLCDNumber()
-        #self.lcd_azi.setFrameShape(QtWidgets.QFrame.NoFrame)
-        #self.lcd_azi.setSmallDecimalPoint(False)
-        #self.lcd_azi.setObjectName("lcd_azi")
-        #self.lcd_azi.setFixedSize(120,50)
-        #''' Elevation '''
-        #self.lcd_ele = QtWidgets.QLCDNumber()
-        #self.lcd_ele.setFrameShape(QtWidgets.QFrame.NoFrame)
-        #self.lcd_ele.setSmallDecimalPoint(False)
-        #self.lcd_ele.setObjectName("lcd_ele")
-        #self.lcd_ele.setFixedSize(120,50)
-        #''' Distance '''
-        #self.lcd_dis = QtWidgets.QLCDNumber()
-        #self.lcd_dis.setFrameShape(QtWidgets.QFrame.NoFrame)
-        #self.lcd_dis.setSmallDecimalPoint(False)
-        #self.lcd_dis.setObjectName("lcd_dis")
-        #self.lcd_dis.setFixedSize(120,50)
-        
+                
         self.lcd_acc = CreateLCD("lcd_acc", False, 120, 50)
         self.lcd_gyr = CreateLCD("lcd_gyr", False, 120, 50)
         self.lcd_mag = CreateLCD("lcd_mag", False, 120, 50)
@@ -109,46 +71,14 @@ class DetailedGraphWidget(QtWidgets.QWidget):
         ''' Graphing '''
         graph = Q3DScatter()
         screenSize = graph.screen().size()
-        self.container = QtWidgets.QWidget.createWindowContainer(graph)
-        self.container.setMinimumSize(QSize(screenSize.width() / 2, screenSize.height() / 1.5))
-        self.container.setMaximumSize(screenSize)
-        self.container.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.container.setFocusPolicy(Qt.StrongFocus)
-        self.modifier = Plotter(graph)
+        self.graph_container = QtWidgets.QWidget.createWindowContainer(graph)
+        self.graph_container.setMinimumSize(QSize(screenSize.width() / 2, screenSize.height() / 1.5))
+        self.graph_container.setMaximumSize(screenSize)
+        self.graph_container.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.graph_container.setFocusPolicy(Qt.StrongFocus)
+        self.modified_graph = Plotter(graph)
         
-        ''' Labels '''
-        #self.label_acc = QtWidgets.QLabel()
-        #self.label_acc.setFixedSize(120,50)
-        #self.label_acc.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.label_acc.setAlignment(QtCore.Qt.AlignCenter)
-        #self.label_acc.setText("Accelerometer")
-        #self.label_gyr = QtWidgets.QLabel()
-        #self.label_gyr.setFixedSize(120,50)
-        #self.label_gyr.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.label_gyr.setAlignment(QtCore.Qt.AlignCenter)
-        #self.label_gyr.setText("Gyroscope")
-        #self.label_mag = QtWidgets.QLabel()
-        #self.label_mag.setFixedSize(120,50)
-        #self.label_mag.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.label_mag.setAlignment(QtCore.Qt.AlignCenter)
-        #self.label_mag.setText("Magnetometer")
-        #self.label_azi = QtWidgets.QLabel()
-        #self.label_azi.setFixedSize(120,50)
-        #self.label_azi.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.label_azi.setAlignment(QtCore.Qt.AlignCenter)
-        #self.label_azi.setText("Azimuth")
-        #self.label_ele = QtWidgets.QLabel()
-        #self.label_ele.setFixedSize(120,50)
-        #self.label_ele.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.label_ele.setAlignment(QtCore.Qt.AlignCenter)
-        #self.label_ele.setText("Elevation")
-        #self.label_dis = QtWidgets.QLabel()
-        #self.label_dis.setFixedSize(120,50)
-        #self.label_dis.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.label_dis.setAlignment(QtCore.Qt.AlignCenter)
-        #self.label_dis.setText("Distance")
-        
-        
+        ''' Labels '''       
         self.label_acc = CreateLabel("Accelerometer", 120, 50)
         self.label_gyr = CreateLabel("Gyroscope", 120, 50)
         self.label_mag = CreateLabel("Magnetometer", 120, 50)
@@ -159,9 +89,11 @@ class DetailedGraphWidget(QtWidgets.QWidget):
 
         ''' Layout '''
         self.horizontal_layout = QtWidgets.QHBoxLayout(self)
+        self.horizontal_layout.addWidget(self.graph_container, 1)
+        
         self.vertical_layout = QtWidgets.QVBoxLayout()
-        self.horizontal_layout.addWidget(self.container, 1)
         self.horizontal_layout.addLayout(self.vertical_layout)
+        
         self.vertical_layout.addWidget(self.run_scan)
         self.vertical_layout.addWidget(self.stop_scan)
         self.vertical_layout.addWidget(self.label_acc)
