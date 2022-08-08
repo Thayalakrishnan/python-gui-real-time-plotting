7bb3b365da4b16e65fd2fb1415f12d893554e950
Lawen <lawen.thaya94@gmail.com>
2022-08-08 20:41:28 +1000

    lazy commit

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # python-gui-real-time-plotting
+
+## About
 A python GUI for plotting real time data over a serial connection
 
+
+
+
+## Commands
+
+isntall dependencies using poetry.
+
+```
+poetry install
+```
+
+```
+poetry run py setup.py
+```
+
+alternatively, using a shell
+
+```
+poetry shell
+py setup.py
+```
+
+launch application
+
+```
+py setup.py
+```
--- realtimeplotter/plotter.py
+++ realtimeplotter/plotter.py
@@ -18,6 +18,15 @@ from qtpy.QtDatavisualization import (Q3DCamera, Q3DScatter, Q3DTheme,
   The class is instantiated within another window and can take in data to be plotted.
   The graph is a scatter plot and is pannable and zoomable. """
   
+def CreateAxis(axis_proxy, axis_title, axis_title_visible, axis_segs, axis_subsegs) -> QValue3DAxis:
+  axis = QValue3DAxis(axis_proxy)
+  axis.setLabelFormat("%.2f mm")
+  axis.setSegmentCount(axis_segs)
+  axis.setSubSegmentCount(axis_subsegs)
+  axis.setTitle(axis_title)
+  axis.setTitleVisible(axis_title_visible)
+  return axis
+
 
 class Plotter(QObject):
     def __init__(self, scatter):
@@ -44,28 +53,31 @@ class Plotter(QObject):
         customTheme.setLabelBackgroundColor(QColor(QColorConstants.Black))
         customTheme.setLabelBackgroundEnabled(True)
         customTheme.setLabelBorderEnabled(True)
-        customTheme.setLabelTextColor(QColor(QColorConstants.White))
+        customTheme.setLabelTextColor(QColor(QColorConstants.Red))
+        
         #light
         customTheme.setLightColor(QColor(QColorConstants.White))
         customTheme.setLightStrength(6.0)
         customTheme.setMultiHighlightColor(QColor(QColorConstants.White))
         customTheme.setSingleHighlightColor(QColor(QColorConstants.White))
-        
-        
+                
         #window 
         #customTheme.setWindowColor(QColor(QColorConstants.Black)) 
         customTheme.setWindowColor(QColor(QColorConstants.White))
         self.graph.activeTheme().setType(Q3DTheme.ThemeUserDefined)
         
-        ''' shadow quality '''
+        ''' font '''
         font = self.graph.activeTheme().font()
         font.setPointSize(12.0)
         self.graph.activeTheme().setFont(QFont("Arial"))
+        
+        ''' shadow quality '''
         self.graph.setShadowQuality(QAbstract3DGraph.ShadowQualitySoftLow)
         self.graph.scene().activeCamera().setCameraPreset(Q3DCamera.CameraPresetIsometricRight)
         self.graph.scene().activeCamera().setZoomLevel(150.0)
         proxy = QScatterDataProxy()
         series = QScatter3DSeries(proxy)
+        
         # this is how an indidiaul point should be labelled when it is clicked on 
         # so the label here will be dipslayed in the graph view when a plotted item is clicked
         series.setItemLabelFormat("@xTitle: @xLabel @yTitle: @yLabel @zTitle: @zLabel")
@@ -73,53 +85,34 @@ class Plotter(QObject):
         series.setItemSize(0.01)
         self.graph.addSeries(series)
         
-        """ Configure axis"""
-        xaxis_proxy = QValue3DAxisFormatter()
-        xaxis = QValue3DAxis(xaxis_proxy)
-        xaxis.setLabelFormat("%.2f mm")
-        xaxis.setSegmentCount(5)
-        xaxis.setSubSegmentCount(2)
-        xaxis.setTitle("X axis")
-        xaxis.setTitleVisible(True)
-        self.graph.addAxis(xaxis)
+        """ Configure axis"""        
+        self.xaxis_proxy = QValue3DAxisFormatter()
+        self.xaxis = CreateAxis(self.xaxis_proxy, "X axis (azimuth)", True, 5, 2)
+        self.graph.addAxis(self.xaxis)
+        self.graph.setAxisX(self.xaxis)
         
-        # configure y axis
-        yaxis_proxy = QValue3DAxisFormatter()
-        yaxis = QValue3DAxis(yaxis_proxy)
-        yaxis.setLabelFormat("%.2f mm")
-        yaxis.setSegmentCount(5)
-        yaxis.setSubSegmentCount(2)
-        yaxis.setTitle("Y axis")
-        yaxis.setTitleVisible(True)
-        self.graph.addAxis(yaxis)
+        self.yaxis_proxy = QValue3DAxisFormatter()
+        self.yaxis = CreateAxis(self.yaxis_proxy, "Y axis (elevation)", True, 5, 2)
+        self.graph.addAxis(self.yaxis)
+        self.graph.setAxisY(self.yaxis)
         
-        # configure z axis
-        zaxis_proxy = QValue3DAxisFormatter()
-        zaxis = QValue3DAxis(zaxis_proxy)
-        zaxis.setLabelFormat("%.2f mm")
-        zaxis.setSegmentCount(5)
-        zaxis.setSubSegmentCount(2)
-        zaxis.setTitle("Z axis")
-        zaxis.setTitleVisible(True)
-        self.graph.addAxis(zaxis)
+        self.zaxis_proxy = QValue3DAxisFormatter()
+        self.zaxis = CreateAxis(self.zaxis_proxy, "Z axis (depth/range)", True, 5, 2)
+        self.graph.addAxis(self.zaxis)
+        self.graph.setAxisZ(self.zaxis)
         
         # title axis??
-        title_proxy = QValue3DAxisFormatter()
-        taxis = QValue3DAxis(title_proxy)
-        taxis.setTitle("Range finder")
-        taxis.setTitleVisible(True)
+        self.title_proxy = QValue3DAxisFormatter()
+        self.taxis = QValue3DAxis(self.title_proxy)
+        self.taxis.setTitle("Range finder")
+        self.taxis.setTitleVisible(True)
         
-        ''' shadow quality '''
-        self.graph.setAxisX(xaxis)
-        self.graph.setAxisY(yaxis)
-        self.graph.setAxisZ(zaxis)
+        ''' Axis Range '''
         self.graph.setTitle("Range finder")
         self.graph.axisX().setRange(-1500,1500)
         self.graph.axisY().setRange(-1500,1500)
         self.graph.axisZ().setRange(0,3000)
-        self.graph.axisX().setTitle("X axis (azimuth)")
-        self.graph.axisY().setTitle("Y axis (elevation)")
-        self.graph.axisZ().setTitle("Z axis (depth/range)")
+        
         self.graph.seriesList()[0].setMesh(self.m_style)
         self.graph.seriesList()[0].setMeshSmooth(self.m_smooth)
         self.graph.setAspectRatio(1.0)
